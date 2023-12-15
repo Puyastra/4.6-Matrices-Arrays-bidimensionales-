5f6559f140435bfc3ca74d60d9ba1f3ed52ff408
Puyastra <140969769+Puyastra@users.noreply.github.com>
2023-12-15 14:17:00 +0100

    Update matrices2.java

--- matrices2.java
+++ matrices2.java
@@ -1,22 +1,23 @@
-/**Ejercicio con el objetivo de crear un programa que cree una matriz de 10x10
-* e introduzca los valores de las tablas de
-* multiplicar del 1 al 10
-*
-*@author Enrique García Herrera
-*/
+/**
+ * Ejercicio con el objetivo de crear un programa que cree una matriz de 10x10
+ * e introduzca los valores de las tablas de
+ * multiplicar del 1 al 10
+ *
+ * @author Enrique García Herrera
+ */
 public class matrices2 {
     public static void main(String[] args) {
-            int[][] matriz = new int[10][10];
-            for(int filas = 0;filas <10; filas++){
-                for(int columnas = 0;columnas<10;columnas++){
-                    matriz[filas][columnas] = (filas +1) * (columnas +1);
-                }
+        int[][] matriz = new int[10][10];
+        for (int filas = 0; filas < 10; filas++) {
+            for (int columnas = 0; columnas < 10; columnas++) {
+                matriz[filas][columnas] = (filas + 1) * (columnas + 1);
             }
-            for(int filas =0; filas<10; filas++){
-                for(int columnas = 0; columnas<10; columnas++){
-                    System.out.print(matriz[filas][columnas] + "\t");
-                }
-                System.out.println();
+        }
+        for (int filas = 0; filas < 10; filas++) {
+            for (int columnas = 0; columnas < 10; columnas++) {
+                System.out.print(matriz[filas][columnas] + "\t");
             }
-    }   
+            System.out.println();
+        }
+    }
 }
