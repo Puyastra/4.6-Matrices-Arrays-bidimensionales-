9e8a8219fafafe1172a06f3e91cb896f30604a52
Puyastra <140969769+Puyastra@users.noreply.github.com>
2023-12-15 14:10:31 +0100

    Update matrices1.java

--- matrices1.java
+++ matrices1.java
@@ -1,3 +1,8 @@
+/**
+*Ejercicio con el objetivo de crear un programa que cree una matriz de tamaño 5x5 que almacene los números del 1 al 25
+*y luego muestre la matriz por pantalla.
+*@author Enrique García Herrera
+*/
 public class matrices1 {
     public static void main(String[] args) {
         int[][] matriz = new int[5][5];
